eb4b172dd80fce4b6bd2a62dfaf224a8da748e09
MishailEx <Mishail.avram@mail.ru>
2021-12-13 13:23:49 +0300

    3. Редактирование вакансии. [#277566]

--- src/main/java/ru/job4j/dream/model/Store.java
+++ src/main/java/ru/job4j/dream/model/Store.java
@@ -17,18 +17,30 @@ public class Store {
         posts.put(1, new Post(1, "Junior Java Job", "easy", "12.02.2021"));
         posts.put(2, new Post(2, "Middle Java Job", "normal", "13.02.2021"));
         posts.put(3, new Post(3, "Senior Java Job", "hard", "14.02.2021"));
-        candidates.put(1, new Candidate(1, "Junior Java"));
-        candidates.put(2, new Candidate(2, "Middle Java"));
-        candidates.put(3, new Candidate(3, "Senior Java"));
+        candidates.put(1, new Candidate(1, "Junior"));
+        candidates.put(2, new Candidate(2, "Middle"));
+        candidates.put(3, new Candidate(3, "Senior"));
     }
 
-    public void save(Post post) {
-        post.setId(POST_ID.incrementAndGet());
+    public void saveCon(Post post) {
+        if (post.getId() == 0) {
+            post.setId(POST_ID.incrementAndGet());
+        }
         posts.put(post.getId(), post);
     }
 
-    public void save(Candidate candidate) {
-        candidate.setId(POST_ID.incrementAndGet());
+    public Post findById(int id) {
+        return posts.get(id);
+    }
+
+    public Candidate condFindById(int id) {
+        return candidates.get(id);
+    }
+
+    public void saveCon(Candidate candidate) {
+        if (candidate.getId() == 0) {
+            candidate.setId(POST_ID.incrementAndGet());
+        }
         candidates.put(candidate.getId(), candidate);
     }
 
