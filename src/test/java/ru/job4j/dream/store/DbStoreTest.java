4a594c9f706d7a8ce0e3ae8d1de3e2b85a802ef3
MishailEx <Mishail.avram@mail.ru>
2021-12-15 17:36:23 +0300

    3. Тесты на DbStore [#504817]

---
+++ src/test/java/ru/job4j/dream/store/DbStoreTest.java
@@ -0,0 +1,135 @@
+package ru.job4j.dream.store;
+
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import ru.job4j.dream.model.Candidate;
+import ru.job4j.dream.model.Post;
+
+import java.io.InputStream;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+import java.util.Collection;
+import java.util.List;
+import java.util.Properties;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.is;
+
+public class DbStoreTest {
+    static Connection connection;
+
+    @BeforeClass
+    public static void initConnection() {
+        try (InputStream in = DbStore.class.getClassLoader().getResourceAsStream("db.properties")) {
+            Properties config = new Properties();
+            config.load(in);
+            Class.forName(config.getProperty("jdbc.driver"));
+            connection = DriverManager.getConnection(
+                    config.getProperty("jdbc.url"),
+                    config.getProperty("jdbc.username"),
+                    config.getProperty("jdbc.password")
+
+            );
+        } catch (Exception e) {
+            throw new IllegalStateException(e);
+        }
+    }
+
+    @AfterClass
+    public static void closeConnection() throws SQLException {
+        connection.close();
+    }
+
+    @After
+    public void wipeTable() throws SQLException {
+        try (PreparedStatement statement = connection.prepareStatement("delete from post; delete from candidate;")) {
+            statement.execute();
+        }
+    }
+
+    @Test
+    public void whenCreatePost() {
+        Store store = DbStore.instOf();
+        Post post = new Post(0, "Java Job");
+        store.save(post);
+        Post postInDb = store.findById(post.getId());
+        assertThat(postInDb.getName(), is(post.getName()));
+    }
+
+    @Test
+    public void whenCreateCandidate() {
+        Store store = DbStore.instOf();
+        Candidate candidate = new Candidate(0, "Oleg");
+        store.save(candidate);
+        Candidate postInDb = store.findByIdCon(candidate.getId());
+        assertThat(postInDb.getName(), is(candidate.getName()));
+    }
+
+    @Test
+    public void whenUpdateCandidate() {
+        Store store = DbStore.instOf();
+        Candidate candidate = new Candidate(0, "Oleg");
+        store.save(candidate);
+        store.save(new Candidate(candidate.getId(), "Max"));
+        Candidate postInDb = store.findByIdCon(candidate.getId());
+        assertThat(postInDb.getName(), is("Max"));
+    }
+
+    @Test
+    public void whenUpdatePost() {
+        Store store = DbStore.instOf();
+        Post post = new Post(0, "Jun");
+        store.save(post);
+        store.save(new Post(post.getId(), "Mid"));
+        Post postInDb = store.findById(post.getId());
+        assertThat(postInDb.getName(), is("Mid"));
+    }
+
+    @Test
+    public void whenFindAllPosts() {
+        Store store = DbStore.instOf();
+        Post post = new Post(0, "Jun");
+        Post post2 = new Post(0, "Mid");
+        store.save(post);
+        store.save(post2);
+        Collection<Post> res = List.of(post, post2);
+        assertThat(store.findAllPosts(), is(res));
+    }
+
+    @Test
+    public void whenFindAllCandidates() {
+        Store store = DbStore.instOf();
+        Candidate candidate = new Candidate(0, "Jun");
+        Candidate candidate1 = new Candidate(0, "Mid");
+        store.save(candidate);
+        store.save(candidate1);
+        Collection<Candidate> res = List.of(candidate, candidate1);
+        assertThat(store.findAllCandidates(), is(res));
+    }
+
+    @Test
+    public void whenFindByIdCandidates() {
+        Store store = DbStore.instOf();
+        Candidate candidate = new Candidate(0, "Jun");
+        Candidate candidate1 = new Candidate(0, "Mid");
+        store.save(candidate);
+        store.save(candidate1);
+        Candidate res = store.findByIdCon(candidate1.getId());
+        assertThat(res.getId(), is(candidate1.getId()));
+    }
+
+    @Test
+    public void whenFindByIdPost() {
+        Store store = DbStore.instOf();
+        Post post = new Post(0, "Jun");
+        Post post1 = new Post(0, "Mid");
+        store.save(post);
+        store.save(post1);
+        Post res = store.findById(post1.getId());
+        assertThat(res.getId(), is(post1.getId()));
+    }
+}
