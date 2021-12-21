b54fa4be93a53e2eeabc6277738f39a009cc83b9
MishailEx <Mishail.avram@mail.ru>
2021-12-21 15:06:44 +0300

    1. Написать тест на PostServlet. [#2518]

--- src/main/java/ru/job4j/dream/servlet/CandidateServlet.java
+++ src/main/java/ru/job4j/dream/servlet/CandidateServlet.java
@@ -2,6 +2,7 @@ package ru.job4j.dream.servlet;
 
 import ru.job4j.dream.model.Candidate;
 import ru.job4j.dream.store.DbStore;
+import ru.job4j.dream.store.Store;
 
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
@@ -10,10 +11,12 @@ import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
 public class CandidateServlet extends HttpServlet {
+    private final static Store STORE = DbStore.instOf();
+
     @Override
-    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws IOException {
         req.setCharacterEncoding("UTF-8");
-        DbStore.instOf().save(new Candidate(
+        STORE.save(new Candidate(
                 Integer.valueOf(req.getParameter("id")),
                 req.getParameter("name")));
         resp.sendRedirect(req.getContextPath() + "/candidates.do");
@@ -21,8 +24,12 @@ public class CandidateServlet extends HttpServlet {
 
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        req.setAttribute("candidates", DbStore.instOf().findAllCandidates());
+        String edit = req.getParameter("edit");
+        String path = edit != null ? "/candidate/edit.jsp" : "/candidates.jsp";
         req.setAttribute("user", req.getSession().getAttribute("user"));
-        req.getRequestDispatcher("candidates.jsp").forward(req, resp);
+        if (edit == null) {
+            req.setAttribute("candidates", STORE.findAllCandidates());
+        }
+        req.getRequestDispatcher(path).forward(req, resp);
     }
 }
--- src/main/java/ru/job4j/dream/servlet/PostServlet.java
+++ src/main/java/ru/job4j/dream/servlet/PostServlet.java
@@ -2,6 +2,7 @@ package ru.job4j.dream.servlet;
 
 import ru.job4j.dream.model.Post;
 import ru.job4j.dream.store.DbStore;
+import ru.job4j.dream.store.Store;
 
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
@@ -10,10 +11,12 @@ import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
 public class PostServlet extends HttpServlet {
+    private final Store store = DbStore.instOf();
+
     @Override
-    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws IOException {
         req.setCharacterEncoding("UTF-8");
-        DbStore.instOf().save(
+        store.save(
                 new Post(
                         Integer.valueOf(req.getParameter("id")),
                         req.getParameter("name")
@@ -24,8 +27,12 @@ public class PostServlet extends HttpServlet {
 
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        req.setAttribute("posts", DbStore.instOf().findAllPosts());
+        String edit = req.getParameter("edit");
+        String path = edit != null ? "/post/edit.jsp" : "/posts.jsp";
         req.setAttribute("user", req.getSession().getAttribute("user"));
-        req.getRequestDispatcher("posts.jsp").forward(req, resp);
+        if (edit == null) {
+            req.setAttribute("posts", store.findAllPosts());
+        }
+        req.getRequestDispatcher(path).forward(req, resp);
     }
 }
